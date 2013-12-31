09bcac42d0978e32a6429cfa5c2fa9749ef1ce53
DragonDon <dragondon@dragondon.net>
2013-12-31 21:15:14 +0900

    Version 2.2.1     - 1.0 flavour text included in code     - 2.0 flavour text moved to external file     - 2.1 change function names to generic names     - 2.2 change print formatting to a function call for flavour text     - 2.2.1 minor text edits, opponent name change, removed "sample" from text name

--- game-core-2.py
+++ game-core-2.py
@@ -1,35 +1,49 @@
 """ DragonDon's Choose Your Own Adventure Core Module
-    Version 1.0
+    Version 2.2.1
+    - 1.0 flavour text included in code
+    - 2.0 flavour text moved to external file
+    - 2.1 change function names to generic names
+    - 2.2 change print formatting to a function call for flavour text
+    - 2.2.1 minor text edits, opponent name change, removed "sample" from text name 
 """
 from sys import exit
 import textwrap
 
 quest = 0
 
+def printText(text):
+    print(textwrap.fill((textwrap.dedent(text).strip()), width=60))
+
+
 def event1(module):
     print ""
-    print textwrap.fill((textwrap.dedent(module.event1_description).strip()), width=60)
-    kobolds_dead = False
+    printText(module.event1_description)
+    opponent1_dead = False
     
     while True:
         print ""
         print module.event1_choice
         next = raw_input(module.event1_prompt)
         
-        if next == module.event1_next1 and not kobolds_dead:
+        if next == module.event1_next1 and not opponent1_dead:
             print ""
-            print dead(textwrap.fill((textwrap.dedent(module.event1_next1_sample_text).strip()), width=60))
-        elif next == module.event1_next2 and not kobolds_dead:
+            printText(module.event1_next1_text)
+            print module.dead_why
+            quest_complete(module)
+        elif next == module.event1_next2 and not opponent1_dead:
             print ""
-            print dead(textwrap.fill((textwrap.dedent(module.event1_next2_sample_text).strip()), width=60))
-        elif next == module.event1_next3 and not kobolds_dead:
+            printText(module.event1_next2_text)
+            print module.dead_why
+            quest_complete(module)
+        elif next == module.event1_next3 and not opponent1_dead:
             print ""
-            print textwrap.fill((textwrap.dedent(module.event1_next3_sample_text).strip()), width=60)
-            kobolds_dead = True
-        elif next == module.event1_next4 and not kobolds_dead:
+            printText(module.event1_next3_text)
+            opponent1_dead = True
+            printText(module.area3_next5_text)
+        elif next == module.event1_next4 and not opponent1_dead:
             print ""
-            print module.event1_next4_sample_text
-        elif next == module.event1_next5 and kobolds_dead:
+            print module.event1_next4_text
+        elif next == module.event1_next5 and opponent1_dead:
             visited_area2 = False
             area1(module, visited_area2)
         else:
@@ -38,28 +52,33 @@ def event1(module):
 
 def event2(module):
     print ""
-    print textwrap.fill((textwrap.dedent(module.event2_description).strip()), width=60)
-    kobolds_dead = False
+    printText(module.event2_description)
+    opponent2_dead = False
     
     while True:
         print ""
         print module.event2_choice
         next = raw_input(module.event2_prompt)
         
-        if next == module.event2_next1 and not kobolds_dead:
+        if next == module.event2_next1 and not opponent2_dead:
             print ""
-            print dead(textwrap.fill((textwrap.dedent(module.event2_next1_sample_text).strip()), width=60))
-        elif next == module.event2_next2 and not kobolds_dead:
+            printText(module.event2_next1_text)
+            print module.dead_why
+            quest_complete(module)
+        elif next == module.event2_next2 and not opponent2_dead:
             print ""
-            print dead(textwrap.fill((textwrap.dedent(module.event2_next2_sample_text).strip()), width=60))
-        elif next == module.event2_next3 and not kobolds_dead:
+            printText(module.event2_next2_text)
+            print module.dead_why
+            quest_complete(module)
+        elif next == module.event2_next3 and not opponent2_dead:
             print ""
-            print textwrap.fill((textwrap.dedent(module.event2_next3_sample_text).strip()), width=60)
-            kobolds_dead = True
-        elif next == module.event2_next4 and not kobolds_dead:
+            printText(module.event2_next3_text)
+            opponent2_dead = True
+            printText(module.area3_next5_text)
+        elif next == module.event2_next4 and not opponent2_dead:
             print ""
-            print module.event2_next4_sample_text
-        elif next == module.event2_next5 and kobolds_dead:
+            print module.event2_next4_text
+        elif next == module.event2_next5 and opponent2_dead:
             visited_area2 = False
             quest_path(module)
         else:
@@ -68,7 +87,7 @@ def event2(module):
             
 def area1(module, visited_area2):
     print ""
-    print textwrap.fill((textwrap.dedent(module.area1_description).strip()), width=60)
+    printText(module.area1_description)
 
     while True:
         print ""
@@ -79,18 +98,18 @@ def area1(module, visited_area2):
             area2(module)
         elif next == module.area1_next2 and visited_area2 == False:
             print ""
-            print textwrap.fill((textwrap.dedent(module.area1_next2_text).strip()), width=60)
+            print (printText(module.area1_next2_text))
         elif next == module.area1_next2 and visited_area2 == True:
             event2(module)
         else:
             print ""
-            print module.event1_next5_text   
+            print (printText(module.event1_next5_text))
 
 def area2(module):
     print ""
-    print textwrap.fill((textwrap.dedent(module.area2_description).strip()), width=60)
+    printText(module.area2_description)
     if quest == 1:
-        print textwrap.fill((textwrap.dedent(module.quest1_sample_text).strip()), width=60)
+        printText(module.quest1_text)
         in_bar = True
 
         while True:
@@ -98,17 +117,17 @@ def area2(module):
             next = raw_input(module.area2_prompt)
             if next == module.area2_next1 and in_bar:
                 print ""
-                print textwrap.fill((textwrap.dedent(module.area2_next1_sample_text).strip()), width=60)
+                print (printText(module.area2_next1_text))
             elif next == module.area2_next2:
                 in_bar = False
                 visited_area2 = True                
                 area1(module, visited_area2)
             else:
                 print ""
-                print module.area2_next2_sample_text
+                print module.area2_next2_text
 
     if quest == 2:
-        print textwrap.fill((textwrap.dedent(module.area2_choice2).strip()), width=60)
+        print module.area2_choice2
         in_bar = True
 
         while True:
@@ -116,20 +135,20 @@ def area2(module):
             next = raw_input(module.area2_prompt)
             if next == module.area2_next3:
                 print ""
-                print textwrap.fill((textwrap.dedent(module.area2_next3_sample_text).strip()), width=60)
+                print (printText(module.area2_next3_text))
             elif next == module.area2_next4 and in_bar:
                 print ""
-                print textwrap.fill((textwrap.dedent(module.area2_next4_sample_text).strip()), width=60)
+                print (printText(module.area2_next4_text))
             elif next == module.area2_next5:
                 visited_area2 = True                
                 area1(module, visited_area2)
             else:
                 print ""
-                print module.area2_next5_sample_text
+                print (printText(module.area2_next5_text))
  
     if quest == 3:
         print ""
-        print textwrap.fill((textwrap.dedent(module.quest3_sample_text).strip()), width=60)
+        printText(module.quest3_text)
         in_bar = True
 
         while True:
@@ -138,158 +157,161 @@ def area2(module):
             next = raw_input(module.area2_prompt)
             if next == module.area2_next6:
                 print ""
-                print textwrap.fill((textwrap.dedent(module.area2_next6_sample_text).strip()), width=60)
+                printText(module.area2_next6_text)
                 next = module.area2_next7
             elif next == module.area2_next7:
                 print ""
-                print textwrap.fill((textwrap.dedent(module.area2_next8_sample_text).strip()), width=60)
+                printText(module.area2_next8_text)
             elif next == module.area2_next9:
                 visited_area2 = True                
                 area1(module, visited_area2)                
             else:
                 print ""
-                print module.area2_next10
+                printText(module.area2_next10)
 
-
-def event3(module):
+def area3(module):
     print ""
-    print textwrap.fill((textwrap.dedent(module.event3_description).strip()), width=60)
+    printText(module.area3_description)
     player_dead = False
 
     while True:
         print ""
-        print module.event3_choice
-        next = raw_input(module.event3_prompt)
+        print module.area3_choice
+        next = raw_input(module.area3_prompt)
         
-        if next == module.event3_next1:
+        if next == module.area3_next1:
             print ""
-            dead((textwrap.fill((textwrap.dedent(module.event3_next1_sample_text).strip()), width=60)), module)
+            printText(module.area3_next1_text)
+            print module.dead_why
             player_dead = True
-            print module.event3_next5_sample_text
-        elif next == module.event3_next2 and not player_dead:
+            print module.area3_next5_text
+        elif next == module.area3_next2 and not player_dead:
             print ""
-            dead(textwrap.fill((textwrap.dedent(module.event3_next2_sample_text).strip()), width=60), module)
+            printText(module.area3_next2_text)
+            print module.dead_why
             player_dead = True
-            print module.event3_next5_sample_text
-        elif next == module.event3_next3 and not player_dead:
+            print module.area3_next5_text
+        elif next == module.area3_next3 and not player_dead:
             print ""
-            dead(textwrap.fill((textwrap.dedent(module.event3_next3_sample_text).strip()), width=60), module)
+            printText(module.area3_next3_text)
+            print module.dead_why
             player_dead = True
-            print module.event3_next5_sample_text
-        elif next == module.event3_next4:
+            print module.area3_next5_text
+        elif next == module.area3_next4:
             print ""
-            dead(textwrap.fill((textwrap.dedent(module.event3_next4_sample_text).strip()), width=60), module)
-            print module.event3_next5_sample_text
+            printText(module.area3_next4_text)
+            print module.dead_why
+            printText(module.area3_next5_text)
             player_dead = True
-            print module.event3_next5_sample_text
-        elif next == module.event3_next6 and not player_dead:
+        elif next == module.area3_next6 and not player_dead:
             print ""
-            print module.event3_next6_sample_text
-        elif next == module.event3_next6 and player_dead:
+            printText(module.area3_next6_text)
+        elif next == module.area3_next6 and player_dead:
             quest_complete(module)
         else:
             print ""
-            print module.event3_next7
+            printText(module.area3_next7)
                 
-def ruins(module):
+def area4(module):
     print ""
-    print textwrap.fill((textwrap.dedent(module.ruins_description).strip()), width=60)
+    printText(module.area4_description)
     death_awaits = True
 
     while True:
-        print module.ruins_choice
-        next = raw_input(module.ruins_prompt)
+        print module.area4_choice
+        next = raw_input(module.area4_prompt)
         
-        if next == module.ruins_next1:
+        if next == module.area4_next1:
             print ""
-            print dead(textwrap.fill((textwrap.dedent(module.ruins_sample_text1).strip()), width=60))
-        elif next == module.ruins_next2 and death_awaits:
+            printText(module.area4_text1)
+            print module.dead_why
+        elif next == module.area4_next2 and death_awaits:
             print ""
-            print dead(textwrap.fill((textwrap.dedent(module.ruins_sample_text2).strip()), width=60))
-        elif next == module.ruins_next3:
+            printText(module.area4_text2)
+            print module.dead_why
+        elif next == module.area4_next3:
             print ""
-            print dead(textwrap.fill((textwrap.dedent(module.ruins_sample_text3).strip()), width=60))
-        elif next == module.ruins_next4 and death_awaits:
+            printText(module.area4_text3)
+            print module.dead_why
+        elif next == module.area4_next4 and death_awaits:
             print ""
-            print dead(textwrap.fill((textwrap.dedent(module.ruins_sample_text4).strip()), width=60))
+            printText(module.area4_text4)
+            print module.dead_why
             death_awaits = False
             quest_complete(module)
         else:
             print ""
-            print module.ruins_sample_text5
+            print module.area4_text5
 
-def cult(module):
+def area5(module):
     print ""
-    print textwrap.fill((textwrap.dedent(module.cult_description).strip()), width=60)
+    printText(module.area5_description)
     death_cult = True
 
     while True:
         print ""
-        print module.cult_choice
+        print module.area5_choice
 
-        next = raw_input(module.cult_prompt)
+        next = raw_input(module.area5_prompt).lower();
         
-        if next == module.cult_next1 and death_cult:
+        if next == module.area5_next1 and death_cult:
             print ""
-            print textwrap.fill((textwrap.dedent(module.cult_sample_text1).strip()), width=60)
-            print module.cult_continue_text
+            printText(module.area5_text1)
+            print module.area5_continue_text
             death_cult = False
-        elif next == module.cult_next2 and death_cult:
+        elif next == module.area5_next2 and death_cult:
             print ""
-            print textwrap.fill((textwrap.dedent(module.cult_sample_text2).strip()), width=60)
-            print module.cult_continue_text
+            printText(module.area5_text2)
+            print module.area5_continue_text
             death_cult = False
-        elif next == module.cult_next3 and death_cult:
+        elif next == module.area5_next3 and death_cult:
             print ""
-            print textwrap.fill((textwrap.dedent(module.cult_sample_text3).strip()), width=60)
-            print module.cult_continue_text
+            printText(module.area5_text3)
+            print module.area5_continue_text
             death_cult = False
-        elif next == module.cult_next4 and death_cult:
+        elif next == module.area5_next4 and death_cult:
             print ""
-            print module.cult_sample_text4
-        elif next == module.cult_next4 and not death_cult:
+            print module.area5_text4
+        elif next == module.area5_next4 and not death_cult:
             quest_complete(module)
         else:
             print ""
-            print module.cult_sample_text5
+            print module.area5_text5
 
 def quest_path(module):
     print ""
     print module.quest_path
     if quest == 1:
-        event3(module)
+        area3(module)
     if quest == 2:    
-        ruins(module)
+        area4(module)
     if quest == 3:       
-        cult(module)
+        area5(module)
 
 def quest_complete(module):
     print ""
-    print textwrap.fill((textwrap.dedent(module.quest_complete_sample_text).strip()), width=60)
+    printText(module.quest_complete_text)
     if quest == 1:
         print ""
-        print textwrap.fill((textwrap.dedent(module.quest_complete_sample_text1).strip()), width=60)
+        printText(module.quest_complete_text1)
         play_again(module)
     if quest == 2:
         print ""
-        print textwrap.fill((textwrap.dedent(module.quest_complete_sample_text2).strip()), width=60)
+        printText(module.quest_complete_text2)
         play_again(module)
     if quest == 3:
         print ""
-        print module.quest_complete_sample_text3
+        print module.quest_complete_text3
         play_again(module)
-
-def dead(why, module):
-    print ""
-    print why, module.dead_why
     
 def start():
     global quest
 
     print "Game Genres to choose from:"
     print "1/ Fantasy"
-    print "2/ Sci-Fi"
-    print "3/ Horror"
+    print "2/ Sci-Fi (Placeholder only)"
+    print "3/ Horror (Placeholder only)"
+    print "or hit any other key + enter to quit"
     genre = raw_input("Choose your game genre: ")
 
     if genre == "1":
@@ -301,8 +323,10 @@ def start():
     elif genre == "3":
         import horror_text
         module = horror_text
+    elif genre == "Q" or "q":
+        exit(0)
     else:
-        print "Please choose a number"
+        print "Please choose an option"
 
     print module.start_text
     next = raw_input(module.start_prompt)
@@ -313,12 +337,13 @@ def start():
         quest = 1
     elif next == module.start_next2:
         quest = 2
-        event1()
+        event1(module)
     elif next == module.start_next3:
         quest = 3
-        event1()     
+        event1(module)     
     else:
-        dead(module.start_dead, module)
+        printText(module.start_dead)
+        print module.dead_why
 
 def play_again(module):
     print ""
@@ -336,7 +361,7 @@ def play_again(module):
             exit(0)
         else:
             print ""
-            print textwrap.fill((textwrap.dedent(module.play_again_sample_text).strip()), width=60)
-            modulequest_complete_sample_text1
+            printText(module.play_again_text)
+            print module.quest_complete_text1
 
 start()
